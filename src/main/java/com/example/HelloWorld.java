32ba0ddd2c736046c27141fb0553cd935b81668c
deepthi-k630 <deepthireddyk630@gmail.com>
2026-06-01 05:23:43 -0700

    build jar

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a demo Java application.");
+        System.out.println("JAR file built successfully!");
+    }
+}
