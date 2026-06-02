330de83cae6875fdf4e7e89c75bd97dd16a1f1ec
deepthi-k630 <deepthireddyk630@gmail.com>
2026-06-02 04:29:51 -0700

    terraform pipeline

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,10 +6,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.50"
     }
   }
-  
+
   backend "s3" {
     bucket = "deepthi630-state-dev"
     key    = "dev.tfstate"
